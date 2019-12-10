ce4145546e4a2c7ca8bc719e3e94a45872d245e2
Sparshith Sampath <sparshithsampath@gmail.com>
2019-12-10 12:23:32 +0530

    fixes

--- mysql/mysql.go
+++ mysql/mysql.go
@@ -3,7 +3,7 @@ package mysql
 import (
 	"fmt"
 
-	"github.com/Sparshith/songski/pkg/config"
+	"github.com/Sparshith/go_common/pkg/config"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
